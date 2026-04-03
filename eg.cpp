997a49708c400f62419d3d22a741214f5fd56c51
HS-hs2007 <439861634@qq.com>
2026-04-03 10:56:42 +0800

    frist commit

--- eg.cpp
+++ eg.cpp
@@ -63,7 +63,7 @@ Status Push(SqStack &S,SElemType e)
  { // 若栈不空，则删除S的栈顶元素，用e返回其值，并返回OK；否则返回ERROR
    if(S.top==S.base)
    return ERROR;
-   e=*--S.top;
+   e=*--S.top;//
    return OK;
  }
 Status StackTraverse(SqStack S,Status(*visit)(SElemType))
